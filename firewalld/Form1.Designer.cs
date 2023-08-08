0a276156c7db28c81b206fda61c96002443b0b56
fanstage <czh18460092659@163.com>
2023-08-08 15:32:05 +0800

    实现了打开和关闭防火墙功能（需要再管理员权限下运行才能实现），获取防火墙状态还在测试中

--- firewalld/Form1.Designer.cs
+++ firewalld/Form1.Designer.cs
@@ -30,46 +30,78 @@ namespace firewalld
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CloseFire = new System.Windows.Forms.Button();
+            this.OpenFire = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Panel();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.AdrBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox5.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(117, 88);
+            this.groupBox1.Controls.Add(this.CloseFire);
+            this.groupBox1.Controls.Add(this.OpenFire);
+            this.groupBox1.Controls.Add(this.status);
+            this.groupBox1.Location = new System.Drawing.Point(37, 88);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(135, 100);
+            this.groupBox1.Size = new System.Drawing.Size(165, 120);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "防火墙服务";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
+            // CloseFire
+            // 
+            this.CloseFire.Location = new System.Drawing.Point(82, 71);
+            this.CloseFire.Name = "CloseFire";
+            this.CloseFire.Size = new System.Drawing.Size(63, 43);
+            this.CloseFire.TabIndex = 1;
+            this.CloseFire.Text = "关闭";
+            this.CloseFire.UseVisualStyleBackColor = true;
+            this.CloseFire.Click += new System.EventHandler(this.CloseFire_Click);
+            // 
+            // OpenFire
+            // 
+            this.OpenFire.Location = new System.Drawing.Point(82, 24);
+            this.OpenFire.Name = "OpenFire";
+            this.OpenFire.Size = new System.Drawing.Size(63, 41);
+            this.OpenFire.TabIndex = 1;
+            this.OpenFire.Text = "打开";
+            this.OpenFire.UseVisualStyleBackColor = true;
+            this.OpenFire.Click += new System.EventHandler(this.OpenFire_Click);
+            // 
+            // status
+            // 
+            this.status.BackColor = System.Drawing.SystemColors.ControlText;
+            this.status.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.status.Location = new System.Drawing.Point(17, 25);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(38, 89);
+            this.status.TabIndex = 0;
+            this.status.Paint += new System.Windows.Forms.PaintEventHandler(this.status_Paint);
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.radioButton2);
             this.groupBox2.Controls.Add(this.radioButton1);
-            this.groupBox2.Location = new System.Drawing.Point(117, 235);
+            this.groupBox2.Location = new System.Drawing.Point(37, 235);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(135, 100);
+            this.groupBox2.Size = new System.Drawing.Size(165, 117);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "防火墙";
             this.groupBox2.Enter += new System.EventHandler(this.groupBox1_Enter);
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(27, 35);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(58, 19);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "打开";
-            this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
             // 
@@ -83,11 +115,22 @@ namespace firewalld
             this.radioButton2.UseVisualStyleBackColor = true;
             this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(27, 35);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "打开";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
             // groupBox3
             // 
-            this.groupBox3.Location = new System.Drawing.Point(379, 88);
+            this.groupBox3.Location = new System.Drawing.Point(266, 88);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(135, 100);
+            this.groupBox3.Size = new System.Drawing.Size(170, 120);
             this.groupBox3.TabIndex = 0;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "防火墙端口";
@@ -95,27 +138,79 @@ namespace firewalld
             // 
             // groupBox4
             // 
-            this.groupBox4.Location = new System.Drawing.Point(379, 235);
+            this.groupBox4.Location = new System.Drawing.Point(266, 235);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(135, 100);
+            this.groupBox4.Size = new System.Drawing.Size(170, 117);
             this.groupBox4.TabIndex = 0;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "防火墙规则";
             this.groupBox4.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label1);
+            this.groupBox5.Controls.Add(this.button2);
+            this.groupBox5.Controls.Add(this.AdrBox);
+            this.groupBox5.Controls.Add(this.button1);
+            this.groupBox5.Location = new System.Drawing.Point(514, 88);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(237, 264);
+            this.groupBox5.TabIndex = 1;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "添加规则";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "文件地址";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(79, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "添加";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // AdrBox
+            // 
+            this.AdrBox.Location = new System.Drawing.Point(79, 24);
+            this.AdrBox.Name = "AdrBox";
+            this.AdrBox.Size = new System.Drawing.Size(152, 25);
+            this.AdrBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -128,6 +223,14 @@ namespace firewalld
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox AdrBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel status;
+        private System.Windows.Forms.Button CloseFire;
+        private System.Windows.Forms.Button OpenFire;
     }
 }
 
--- firewalld/Form1.cs
+++ firewalld/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFirewallHelper;
 
 namespace firewalld
 {
@@ -25,6 +27,64 @@ namespace firewalld
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void CloseFire_Click(object sender, EventArgs e)
+        {
+            //关闭防火墙
+            Process proc = new Process();
+            string top = "netsh.exe";
+            proc.StartInfo.Arguments = "Advfirewall set allprofiles state off";
+            proc.StartInfo.FileName = top;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.Start();
+            proc.WaitForExit();
+            status.BackColor = Color.Red;
+        }
+
+        private void OpenFire_Click(object sender, EventArgs e)
+        {
+            //打开防火墙
+            Process proc = new Process();
+            string top = "netsh.exe";
+            proc.StartInfo.Arguments = "Advfirewall set allprofiles state on";
+            proc.StartInfo.FileName = top;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.Start();
+            proc.WaitForExit();
+            status.BackColor = Color.Green;
+        }
+
+        private void status_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private bool IsFirewallEnabled()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "netsh.exe";
+            process.StartInfo.Arguments = "advfirewall show currentprofile state";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return output.Contains("ON");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (IsFirewallEnabled())
+            {
+                status.BackColor = Color.Green;
+            }
+            else
+            {
+                status.BackColor = Color.Red;
+            }
         }
     }
 }
